af855cfebcd206e49a7482cb2c95743cc9671b12
Alexander Ebert <ebert@woltlab.com>
2012-07-02 13:40:05 +0200

    Removed debug code

--- files/lib/system/dashboard/DashboardHandler.class.php
+++ files/lib/system/dashboard/DashboardHandler.class.php
@@ -107,7 +107,7 @@ class DashboardHandler extends SingletonFactory {
 		$objectTypeID = 0;
 		
 		// get object type id (cache might be outdated)
-		if (false && PACKAGE_ID && PACKAGE_ID != 1) {
+		if (PACKAGE_ID && PACKAGE_ID != 1) {
 			// reset object type cache
 			// TODO: Add a method to ObjectType in order to clear cache
 			CacheHandler::getInstance()->clear(WCF_DIR.'cache/', 'cache.objectType-*.php');
