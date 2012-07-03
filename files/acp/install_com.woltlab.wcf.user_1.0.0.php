3da99e98efc7ce73f44b5eb55a3207fb0f036c7c
Alexander Ebert <ebert@woltlab.com>
2012-07-03 19:25:28 +0200

    Unified scripts PIP

--- files/acp/install_com.woltlab.wcf.user_1.0.0.php
+++ files/acp/install_com.woltlab.wcf.user_1.0.0.php
@@ -6,4 +6,4 @@ use wcf\system\dashboard\DashboardHandler;
  * @copyright	2001-2012 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
-DashboardHandler::setDefaultValues('com.woltlab.wcf.user.DashboardPage', array('recentActivity'));
+DashboardHandler::setDefaultValues('com.woltlab.wcf.user.DashboardPage', array('recentActivity' => 1));
