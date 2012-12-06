211b8b5497773e3410a5853f5cd12508ea2e3c70
Marcel Werk <burntime@woltlab.com>
2012-12-06 21:01:46 +0100

    Fixed cache builder

--- files/lib/form/SettingsForm.class.php
+++ files/lib/form/SettingsForm.class.php
@@ -87,7 +87,7 @@ class SettingsForm extends AbstractForm {
 		if (!empty($_REQUEST['category'])) $this->category = $_REQUEST['category'];
 		// todo validate category
 		
-		$this->optionHandler = new UserOptionHandler('user-option', 'wcf\system\cache\builder\OptionCacheBuilder', false, '', 'settings.'.$this->category, false);
+		$this->optionHandler = new UserOptionHandler('userOption', 'wcf\system\cache\builder\OptionCacheBuilder', false, '', 'settings.'.$this->category, false);
 		$this->optionHandler->setUser(WCF::getUser());
 		
 		if ($this->category == 'general') {
