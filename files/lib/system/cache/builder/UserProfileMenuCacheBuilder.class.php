86cf20e58b5e6910c6c5c133183b9664c708ab39
Alexander Ebert <ebert@woltlab.com>
2013-01-10 14:16:35 +0100

    Optimized cache builder for user profile menu

--- files/lib/system/cache/builder/UserProfileMenuCacheBuilder.class.php
+++ files/lib/system/cache/builder/UserProfileMenuCacheBuilder.class.php
@@ -1,13 +1,12 @@
 <?php
 namespace wcf\system\cache\builder;
-use wcf\data\user\profile\menu\item\UserProfileMenuItem;
-use wcf\system\WCF;
+use wcf\data\user\profile\menu\item\UserProfileMenuItemList;
 
 /**
  * Caches the user profile menu items.
  * 
  * @author	Alexander Ebert
- * @copyright	2001-2012 WoltLab GmbH
+ * @copyright	2001-2013 WoltLab GmbH
  * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @package	com.woltlab.wcf
  * @subpackage	system.cache.builder
@@ -18,19 +17,11 @@ class UserProfileMenuCacheBuilder implements ICacheBuilder {
 	 * @see	wcf\system\cache\ICacheBuilder::getData()
 	 */
 	public function getData(array $cacheResource) { 
-		$data = array();
+		$itemList = new UserProfileMenuItemList();
+		$itemList->sqlLimit = 0;
+		$itemList->sqlOrderBy = "user_profile_menu_item.showOrder ASC";
+		$itemList->readObjects();
 		
-		$sql = "SELECT		menuItemID, menuItem, permissions, options, packageDir, menu_item.packageID, className
-			FROM		wcf".WCF_N."_user_profile_menu_item menu_item
-			LEFT JOIN	wcf".WCF_N."_package package
-			ON		(package.packageID = menu_item.packageID)
-			ORDER BY	showOrder ASC";
-		$statement = WCF::getDB()->prepareStatement($sql);
-		$statement->execute();
-		while ($row = $statement->fetchArray()) {
-			$data[] = new UserProfileMenuItem(null, $row);
-		}
-				
-		return $data;
+		return $itemList->getObjects();
 	}
 }
