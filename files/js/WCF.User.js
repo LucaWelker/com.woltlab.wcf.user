2ba5d37e925f2bab7d6e117c9c997798186204c0
Alexander Ebert <ebert@woltlab.com>
2013-01-29 00:13:45 +0100

    Fixed multiple editing of user profile
    Fixes #153
    Fixes #171

--- files/js/WCF.User.js
+++ files/js/WCF.User.js
@@ -556,6 +556,8 @@ WCF.User.Profile.Editor = Class.extend({
 		this._actionName = 'restore';
 		this._buttons.beginEdit.show();
 		
+		this._destroyCKEditor();
+		
 		this._tab.html(this._cachedTemplate).children().css({ height: 'auto' });
 	},
 	
@@ -579,7 +581,7 @@ WCF.User.Profile.Editor = Class.extend({
 					this._restore();
 				}
 				else {
-					this._prepareEdit(data);
+					this._prepareEdit(data, true);
 				}
 			break;
 		}
@@ -589,12 +591,18 @@ WCF.User.Profile.Editor = Class.extend({
 	 * Prepares editing mode.
 	 * 
 	 * @param	object		data
+	 * @param	boolean		disableCache
 	 */
-	_prepareEdit: function(data) {
+	_prepareEdit: function(data, disableCache) {
+		this._destroyCKEditor();
+		
 		// update template
 		var self = this;
 		this._tab.html(function(index, oldHTML) {
-			self._cachedTemplate = oldHTML;
+			if (disableCache !== true) {
+				self._cachedTemplate = oldHTML;
+			}
+			
 			return data.returnValues.template;
 		});
 		
@@ -607,6 +615,19 @@ WCF.User.Profile.Editor = Class.extend({
 				
 				event.preventDefault();
 				return false;
+			}
+		});
+	},
+	
+	/**
+	 * Destroys all CKEditor instances within current tab.
+	 */
+	_destroyCKEditor: function() {
+		// destroy all CKEditor instances
+		this._tab.find('textarea + .cke').each(function(index, container) {
+			var $instanceName = $(container).attr('id').replace(/cke_/, '');
+			if (CKEDITOR.instances[$instanceName]) {
+				CKEDITOR.instances[$instanceName].destroy();
 			}
 		});
 	}
