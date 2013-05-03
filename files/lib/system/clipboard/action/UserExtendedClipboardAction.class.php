95edea86c837278ad317d1bda907a0ef9f3ba621
Marcel Werk <burntime@woltlab.com>
2013-05-03 02:04:37 +0200

    Added user merge (WIP)

---
+++ files/lib/system/clipboard/action/UserExtendedClipboardAction.class.php
@@ -0,0 +1,72 @@
+<?php
+namespace wcf\system\clipboard\action;
+use wcf\system\request\LinkHandler;
+use wcf\system\WCF;
+
+/**
+ * Prepares clipboard editor items for user objects.
+ * 
+ * @author	Alexander Ebert
+ * @copyright	2001-2013 WoltLab GmbH
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package	com.woltlab.wcf.user
+ * @subpackage	system.clipboard.action
+ * @category	Community Framework
+ */
+class UserExtendedClipboardAction extends AbstractClipboardAction {
+	/**
+	 * @see	wcf\system\clipboard\action\AbstractClipboardAction::$supportedActions
+	 */
+	protected $supportedActions = array('merge');
+	
+	/**
+	 * @see	wcf\system\clipboard\action\IClipboardAction::execute()
+	 */
+	public function execute(array $objects, $actionName) {
+		$item = parent::execute($objects, $actionName);
+		
+		if ($item === null) {
+			return null;
+		}
+		
+		// handle actions
+		switch ($actionName) {
+			case 'merge':
+				$item->setURL(LinkHandler::getInstance()->getLink('UserMerge'));
+			break;
+		}
+		
+		return $item;
+	}
+	
+	/**
+	 * @see	wcf\system\clipboard\action\IClipboardAction::getClassName()
+	 */
+	public function getClassName() {
+		return 'wcf\data\user\UserAction';
+	}
+	
+	/**
+	 * @see	wcf\system\clipboard\action\IClipboardAction::getTypeName()
+	 */
+	public function getTypeName() {
+		return 'com.woltlab.wcf.user';
+	}
+	
+	/**
+	 * Returns the ids of the users which can be merge.
+	 *
+	 * @return	array<integer>
+	 */
+	protected function validateMerge() {
+		// check permissions
+		if (!WCF::getSession()->getPermission('admin.user.canEditUser')) {
+			return array();
+		}
+		
+		$userIDs = array_keys($this->objects);
+		if (count($userIDs) < 2) return array();
+		
+		return $userIDs;
+	}
+}
